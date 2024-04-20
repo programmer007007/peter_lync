c66e38b437af6f9226404e6e771a023a3fe34e51
programmer007007 <35029477+programmer007007@users.noreply.github.com>
2024-04-20 10:05:17 +0530

    Update PeterLyncFormula-2024-04-20.user.js

--- PeterLyncFormula-2024-04-20.user.js
+++ PeterLyncFormula-2024-04-20.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         PeterLyncFormula
+// @name         PeterLynchFormula
 // @namespace    http://tampermonkey.net/
 // @version      2024-04-20
 // @description  try to take over the world!
-// @author       You
+// @author       Johnson
 // @match        https://finance.yahoo.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yahoo.com
 // @grant        none
